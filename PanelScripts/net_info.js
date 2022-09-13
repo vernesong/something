b64e94bde1a672e1c141d918c9be46e6ead415cb
vernesong <42875168+vernesong@users.noreply.github.com>
2022-09-13 23:41:42 +0800

    chore: refine

--- PanelScripts/net_info.js
+++ PanelScripts/net_info.js
@@ -17,14 +17,12 @@ while(allGroup.includes(rootName)==true){
 
 $httpClient.get('http://ip-api.com/json/?lang=en', function (error, response, data) {
     if (error != null || response.status !== 200) {
-      reject('Error')
-      return
+      $done();
     }
     
     const jsonData = JSON.parse(data);
     if(jsonData?.errors){
-      reject('Not Available')
-      return
+      $done();
     }
 
     $done({
